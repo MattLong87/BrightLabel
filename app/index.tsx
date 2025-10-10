c9cfa196c2d23b1fa81a8d07d54b483c2b2d4ff3
Matthew Long <mattlong87@gmail.com>
2025-10-10 11:14:19 -0400

    styling camera screen, add haptics

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,14 @@
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 import { useFocusEffect } from '@react-navigation/native';
 import { BarcodeScanningResult, CameraType, CameraView, useCameraPermissions } from 'expo-camera';
+import * as Haptics from 'expo-haptics';
 import { useRouter } from 'expo-router';
 import { useCallback, useRef, useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   const [facing, setFacing] = useState<CameraType>('back');
+  const [light, setLight] = useState<boolean>(false);
   const [permission, requestPermission] = useCameraPermissions();
   const barcodeDetected = useRef<boolean>(false);
   const router = useRouter();
@@ -32,23 +35,36 @@ export default function App() {
   }
 
   function toggleCameraFacing() {
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Rigid)
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  function toggleCameraLight() {
+    Haptics.notificationAsync(
+      Haptics.NotificationFeedbackType.Success
+    )
+    setLight(current => !current);
+  }
+
   function onBarcodeScanned(event: BarcodeScanningResult) {
-    if(barcodeDetected.current) {
+    if (barcodeDetected.current) {
       return;
     }
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
     barcodeDetected.current = true;
     return router.push(`/productinfo/${event.data}` as any);
   }
 
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing} barcodeScannerSettings={{ barcodeTypes: ["upc_a", "ean13"], }} onBarcodeScanned={onBarcodeScanned} />
+      <CameraView style={styles.camera} facing={facing} barcodeScannerSettings={{ barcodeTypes: ["upc_a", "ean13"], }} onBarcodeScanned={onBarcodeScanned} enableTorch={light} />
+      <View style={styles.guideBoxContainer}><View style={styles.guideBox} /><Text style={styles.guideBoxText}>Scan a barcode</Text></View>
       <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-          <Text style={styles.text}>Flip Camera</Text>
+        <TouchableOpacity style={[styles.button, facing === 'front' && styles.buttonActive]} onPress={toggleCameraFacing}>
+          <MaterialCommunityIcons name="camera-flip" size={30} color="#e4e4e4" />
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, light && styles.buttonActive]} onPress={toggleCameraLight}>
+          <MaterialCommunityIcons name="flashlight" size={30} color="#e4e4e4" />
         </TouchableOpacity>
       </View>
     </View>
@@ -67,17 +83,53 @@ const styles = StyleSheet.create({
   camera: {
     flex: 1,
   },
+  guideBoxContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+  guideBox: {
+    width: '70%',
+    height: 200,
+    borderWidth: 2,
+    borderColor: '#fff',
+    borderRadius: 24,
+  },
+  guideBoxText: {
+    fontSize: 16,
+    color: '#fff',
+    textAlign: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingTop: 8,
+    paddingBottom: 8,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 40,
+  },
   buttonContainer: {
     position: 'absolute',
-    bottom: 64,
+    bottom: 32,
     flexDirection: 'row',
     backgroundColor: 'transparent',
     width: '100%',
-    paddingHorizontal: 64,
+    paddingHorizontal: 32,
+    justifyContent: 'space-between',
   },
   button: {
-    flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    borderRadius: 100,
+    borderWidth: 1,
+    borderColor: '#e4e4e4',
+  },
+  buttonActive: {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
   },
   text: {
     fontSize: 24,
